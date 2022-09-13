b6c5d5f4a2b836058ba4f7abeef156b4e7529888
Bice93 <bianca.cerulo@hotmail.it>
2022-09-13 22:21:03 +0200

    Style comics.show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,19 +4,19 @@
 
 @section('main-content')
     <div class="container">
-        <div class="row">
-            <div class="card mb-3" style="max-width: 540px;">
+        <div class="row py-5">
+            <div class="card p-0">
                 <div class="row g-0">
-                  <div class="col-md-4">
-                    <img src="{{ $comic->image_url }}" class="img-fluid rounded-start" alt="{{ $comic->title }}'s image">
+                  <div class="col-md-4 box-image-card">
+                    <img src="{{ $comic->image_url }}" class="rounded-start" alt="{{ $comic->title }}'s image">
                   </div>
-                  <div class="col-md-8">
+                  <div class="col-md-8 box-info-card">
                     <div class="card-body">
                       <h4 class="card-title">{{ $comic->title }}</h4>
                       <h6 class="card-title">{{ $comic->series }}</h6>
-                      <h6 class="card-title">Genere: {{ $comic->type }}</h6>
+                      <h6 class="card-title"><span class="fw-normal">Genere:</span> {{ $comic->type }}</h6>
                       <p class="card-text"> {{ $comic->description }}</p>
-                      <p class="card-text">Prima pubblicazione: {{ $comic->sale_date }}</p>
+                      <p class="card-text"><span class="fw-normal">Pubblicazione:</span> {{ $comic->sale_date }}</p>
                       <p class="card-text"><small class="text-muted">€ {{ $comic->price }}</small></p>
                     </div>
                   </div>
