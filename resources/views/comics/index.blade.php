0e1f9191de144c387e70d07046f9d6f094470017
Bice93 <bianca.cerulo@hotmail.it>
2022-09-13 21:46:53 +0200

    Style comics.index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,29 +4,31 @@
 
 @section('main-content')
     <div class="container">
-        <div class="row">
-            <table class="table">
-                <thead class="table-light">
-                    <th>ID</th>
-                    <th colspan="3">Title</th>
-                    <th>Series</th>
-                    <th>Type</th>
-                </thead>
-                <tbody>
-                    @forelse ($comics as $comic)
-                        <tr>
-                            <td>{{ $comic->id }}</td>
-                            <td colspan="3"><a href="{{ route('comics.show', $comic->id) }}">{{ $comic->title }}</a></td>
-                            <td>{{ $comic->series }}</td>
-                            <td>{{ $comic->type }}</td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td>Non sono disponibili fumetti da visualizzare!</td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
+        <div class="row py-4">
+            <div class="box_table">
+                <table class="table style_table">
+                    <thead class="table-light">
+                        <th>ID</th>
+                        <th colspan="3">Title</th>
+                        <th>Series</th>
+                        <th>Type</th>
+                    </thead>
+                    <tbody>
+                        @forelse ($comics as $comic)
+                            <tr>
+                                <td>{{ $comic->id }}</td>
+                                <td colspan="3"><a href="{{ route('comics.show', $comic->id) }}">{{ $comic->title }}</a></td>
+                                <td>{{ $comic->series }}</td>
+                                <td>{{ $comic->type }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td>Non sono disponibili fumetti da visualizzare!</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
