d98c60d78bd5edc195c37fd2339af63912ac6927
Bice93 <bianca.cerulo@hotmail.it>
2022-09-13 15:10:25 +0200

    Organization of views

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Main comics')
+
+@section('main-content')
+    <h1>Admin homepage</h1>    
+@endsection
---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Main comics')
+
+@section('main-content')
+    <h1>Main content comics</h1>    
+@endsection
