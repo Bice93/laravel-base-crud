6722ccd45715ff8a09f0632ce398a8d0e83a526c
Bice93 <bianca.cerulo@hotmail.it>
2022-09-13 17:21:29 +0200

    Create table in comics.index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,5 +3,30 @@
 @section('title', 'Main comics')
 
 @section('main-content')
-    <h1>Main content comics</h1>
+    <div class="container">
+        <div class="row">
+            <table class="table">
+                <thead class="table-light">
+                    <th>ID</th>
+                    <th colspan="3">Title</th>
+                    <th>Series</th>
+                    <th>Type</th>
+                </thead>
+                <tbody>
+                    @forelse ($comics as $comic)
+                        <tr>
+                            <td>{{ $comic->id }}</td>
+                            <td colspan="3"><a href="#">{{ $comic->title }}</a></td>
+                            <td>{{ $comic->series }}</td>
+                            <td>{{ $comic->type }}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td>Non sono disponibili fumetti da visualizzare!</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
