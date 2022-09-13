81b9c15fdb6bfd4d1777fe07b55e303916dce559
Bice93 <bianca.cerulo@hotmail.it>
2022-09-13 21:16:36 +0200

    Style admin.home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,5 +3,12 @@
 @section('title', 'Main comics')
 
 @section('main-content')
-    <h1>Admin homepage</h1>    
+    <div class="container">
+        <div class="row text-center py-4">
+            <h1 class="title_homepage">Archivio Fumetti</h1>
+        </div>
+        <div class="row py-4">
+            <p class="fs-1 fw-lighter fst-italic">Welcome to admin homepage, use the top navigation bar to navigate!</p>
+        </div>
+    </div>    
 @endsection
