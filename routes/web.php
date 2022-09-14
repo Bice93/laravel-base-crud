17104b47e5a08b47c35782944ef2afc7f65b24d9
Bice93 <bianca.cerulo@hotmail.it>
2022-09-14 16:51:25 +0200

    Add other method route in web

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index')->name('home');
 // Route::get('/comics', 'ComicController@index')->name('comics.index');
+// Route::get('/comics/create', 'ComicController@create')->name('comics.create');
+// Route::post('/comics', 'ComicController@store')->name('comics.store');
 // Route::get('/comics/{id}', 'ComicController@show')->name('comics.show');
 Route::resource('/comics', 'ComicController');
 
