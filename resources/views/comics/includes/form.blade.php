9562cfd1a28176064177f535768bf7dda57f2037
Bice93 <bianca.cerulo@hotmail.it>
2022-09-16 17:54:54 +0200

    Add new messageError.blade

--- resources/views/comics/includes/form.blade.php
+++ resources/views/comics/includes/form.blade.php
@@ -1,9 +1,10 @@
 <div class="mb-3">
     <label for="input-title" class="form-label">Title</label>
     <input type="text" name="title" value="{{ old('title', $comic->title) }}" class="form-control" id="input-title">
-    @error('title')
+    {{-- @error('title')
         <div class="alert alert-danger mt-1 w-50 style_error_message">{{ $message }}</div>
-    @enderror
+    @enderror --}}
+    @include('comics.includes.messageError', ['name' => 'title'])
 
 </div>
 
@@ -11,9 +12,10 @@
     <label for="input-series" class="form-label">Series</label>
     <input type="text" name="series" value="{{ old('series', $comic->series) }}" class="form-control"
         id="input-series">
-    @error('series')
+    {{-- @error('series')
         <div class="alert alert-danger mt-1 w-50 style_error_message">{{ $message }}</div>
-    @enderror
+    @enderror --}}
+    @include('comics.includes.messageError', ['name' => 'series'])
 </div>
 
 <div class="mb-3">
@@ -27,9 +29,10 @@
             </option>
         @endforeach
     </select>
-    @error('type')
+    {{-- @error('type')
         <div class="alert alert-danger mt-1 w-50 style_error_message">{{ $message }}</div>
-    @enderror
+    @enderror --}}
+    @include('comics.includes.messageError', ['name' => 'type'])
 </div>
 
 <div class="mb-3">
@@ -37,36 +40,40 @@
     <textarea type="text" name="description" class="form-control" id="textarea-description">
         {{ old('description', $comic->description) }}
     </textarea>
-    @error('description')
+    {{-- @error('description')
         <div class="alert alert-danger mt-1 w-50 style_error_message">{{ $message }}</div>
-    @enderror
+    @enderror --}}
+    @include('comics.includes.messageError', ['name' => 'description'])
 </div>
 
 <div class="mb-3">
     <label for="input-image" class="form-label">Image</label>
     <input type="text" name="image_url" value="{{ old('image_url', $comic->image_url) }}" class="form-control"
         id="input-image">
-    @error('image_url')
+    {{-- @error('image_url')
         <div class="alert alert-danger mt-1 w-50 style_error_message">{{ $message }}</div>
-    @enderror
+    @enderror --}}
+    @include('comics.includes.messageError', ['name' => 'image_url'])
 </div>
 
 <div class="mb-3">
     <label for="input-price" class="form-label">Price</label>
     <input type="text" name="price" value="{{ old('price', $comic->price) }}" class="form-control"
         id="input-price">
-    @error('price')
+    {{-- @error('price')
         <div class="alert alert-danger mt-1 w-50 style_error_message">{{ $message }}</div>
-    @enderror
+    @enderror --}}
+    @include('comics.includes.messageError', ['name' => 'price'])
 </div>
 
 <div class="mb-3">
     <label for="input-date" class="form-label">Date</label>
     <input type="date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}" class="form-control"
         id="input-date">
-    @error('sale_date')
+    {{-- @error('sale_date')
         <div class="alert alert-danger mt-1 w-50 style_error_message">{{ $message }}</div>
-    @enderror
+    @enderror --}}
+    @include('comics.includes.messageError', ['name' => 'sale_date'])
 </div>
 
 <button type="submit" class="btn btn-primary">Edit element</button>
